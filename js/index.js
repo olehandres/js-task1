fc95247119f843e203fabe18b030f7db67142696
Oleh Andres <olehandres@icloud.com>
2018-10-25 21:09:40 +0000

    Create new project

---
+++ js/index.js
@@ -0,0 +1,49 @@
+/*const a = parseFloat(prompt('Enter A'));
+const b = parseFloat(prompt('Enter B'));
+const c = parseFloat(prompt('Enter C'));
+// const aa = 2;
+// const bb = 22;
+// const cc = 2;
+
+function diskr(a, b, c) {
+    return ((b * b) - (4 * a * c));
+}
+
+function quadraticEq(a, b, c) {
+    const D = diskr(a, b, c);
+    console.log(D)
+    if (D > 0) {
+        const x1 = (-b + Math.sqrt(D)) / (2 * a);
+        const x2 = (-b - Math.sqrt(D)) / (2 * a);
+        alert('x1=' + x1 + 'x2 =' + x2);
+    } else if (D == 0) {
+        const x1 = (-b) / (2 * a);
+        alert('x =' + x1);
+    } else if (D < 0) {
+        alert('No solution');
+    }
+}
+
+quadraticEq(a, b, c);
+*/
+
+
+console.log(this);
+
+
+function blender (fruit) {
+    const juice =  fruit + ' juice';
+    return juice;
+}
+
+
+var glass = blender('orange');
+
+
+
+
+
+
+
+
+
